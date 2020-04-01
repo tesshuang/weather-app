d327625d4ef05b607b121552fcc0f8ce47b9f929
Tess Huang <tesshuangxj@yahoo.com>
2020-03-31 21:27:44 -0700

    Added date to result comp

--- src/components/Result.js
+++ src/components/Result.js
@@ -5,11 +5,13 @@ export default class Result extends React.Component {
   render() {
     const { name, weather, main, wind } = this.props.weatherData
     const curWeather = weather[0]
+    const today = new Date().toString().slice(0, 15);
 
     return(
       <div>
         <div className='result-wrapper'>
           <h4 className='name'>{name}</h4>
+          <p className='date'>{today}</p>
           <div className='general'>
             <img src={`http://openweathermap.org/img/wn/${curWeather.icon}@2x.png`} alt={`current weather of ${name}`}/>
             <p className='temp'>{`${main.temp}°`}</p>
